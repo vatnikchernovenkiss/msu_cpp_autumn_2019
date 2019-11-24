9bff4a642665f2cb238ab24c954fd2b2322363ca
vatnikchernovenkiss <56398346+vatnikchernovenkiss@users.noreply.github.com>
2019-11-24 15:23:43 +0300

    Add files via upload

--- 07/vector.h
+++ 07/vector.h
@@ -176,8 +176,9 @@ private:
     pointer data;
     size_type cur_size;
 public:
+
 	Vector(size_type size = 0):alloc(), allocated(base_size),
-		data(alloc.allocate(allocated)), cur_size(size)
+	data(alloc.allocate(allocated)), cur_size(size)
     {
         for (pointer ptr = data; ptr != data + size; ++ptr) {
             alloc.construct(ptr);
