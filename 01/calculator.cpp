977bba9f00423e57b602cd9ca6d88100ad8e4c73
vatnikchernovenkiss <56398346+vatnikchernovenkiss@users.noreply.github.com>
2019-12-12 22:08:21 +0300

    Add files via upload

--- 01/calculator.cpp
+++ 01/calculator.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 class Error {
-	const char *mes;
+	string mes;
 public:
 	Error(const char* mes): mes(mes){}
-	void get() const {
-		cout<<mes;
+	const string& get() const {
+		return mes;
 	}
 };
 /*S->T{+T | -T}
@@ -17,10 +17,11 @@ T->F{*F | /F}
 F-> number
 */
 
-void space_skip(string::const_iterator &current) {
-	while (isspace(*current)) {
-		current++;
+void space_skip(const string& input, int &pos) {
+	while (isspace(input[pos])) {
+		++pos;
 	}
+
 }
 
 void prep(int &second_op, int &first_op, stack<int> &st) {
@@ -30,50 +31,60 @@ void prep(int &second_op, int &first_op, stack<int> &st) {
 	st.pop();
 }
 
-void get_digit(stack<int> &st, string::const_iterator &current)
+void get_digit(stack<int> &st, const string& input, int &pos)
 {
 	int res = 0, fl = 1;
-	if (!isdigit(*current)) {
-		while (*current == '-') {
+	if (!isdigit(input[pos])) {
+		while (input[pos] == '-') {
 			fl *= -1;
-			space_skip(++current);
+			space_skip(input, ++pos);
 		}
-		if (!isdigit(*current)) {
-			throw Error("error");
+		if (!isdigit(input[pos])) {
+			if (input[pos] == '+' || input[pos] == '-' || input[pos] == '*' ||
+			input[pos] == '/' || input[pos] == '\0') {
+				throw Error("Incorrect using of operation");
+			} else {
+				throw Error("Incorrect symbol found");
+			}
 		}
 	}		
-	while (isdigit(*current)) {
-		res = res * 10 + *current++ - '0';
+	while (isdigit(input[pos])) {
+		res = res * 10 + input[pos++] - '0';
 	}
 	st.push(fl * res);
-	space_skip(current);
+
+	space_skip(input, pos);
+
 }
 
-void mul_div_parse(stack<int> &st, string::const_iterator &current)
+void mul_div_parse(stack<int> &st, const string& input, int &pos)
 {
-	get_digit(st, current);
+	get_digit(st, input, pos);
 	char buf;
-	while ((buf = *current) == '*' || buf == '/') {
-		space_skip(++current);
-		get_digit(st, current);
+	while ((buf = input[pos]) == '*' || buf == '/') {
+		space_skip(input, ++pos);
+		get_digit(st, input, pos);
 		int second_op = 0,  first_op = 0;
 		prep(second_op,first_op, st);
 		if (buf == '*') {
 			st.push(second_op * first_op);
 		} else {
+			if (!second_op) {
+				throw Error("Division by zero!");
+			}
 			st.push(first_op / second_op);
 		}
 	}
 }
 
-void add_sub_parse(stack<int> &st, string::const_iterator &current)
+void add_sub_parse(stack<int> &st, const string& input, int &pos)
 {
-	space_skip(current);
-	mul_div_parse(st, current);
+	space_skip(input, pos);
+	mul_div_parse(st, input, pos);
 	char buf;
-	while ((buf = *current) == '+' || buf == '-') {
-		space_skip(++current);
-		mul_div_parse(st, current);
+	while ((buf = input[pos]) == '+' || buf == '-') {
+		space_skip(input, ++pos);
+		mul_div_parse(st, input, pos);
 		int second_op = 0,  first_op= 0;
 		prep(second_op, first_op, st);
 		if (buf == '+') {
@@ -82,25 +93,24 @@ void add_sub_parse(stack<int> &st, string::const_iterator &current)
 			st.push(first_op - second_op);
 		}
 	}
-	if (*current != '\0') {
-		throw Error("error");
+	if (input[pos] != '\0') {
+		throw Error("Incorrect symbol found");
 	}
 }
 
 int main(int argc, char **argv) 
 {
 	stack<int> st;
+	int pos = 0;
 	string input(argv[1]);
-	string::const_iterator current = input.begin();
 	try {
-		add_sub_parse(st,current);
+		add_sub_parse(st,input, pos);
 	} 
-	catch(Error res) {
-		res.get();
+	catch(Error &res) {
+		cout<<res.get()<<endl;
 		return 1;
 	}
-	cout<<st.top();
+	cout<<st.top()<<endl;
 	return 0;
 }
 	
-
